2e175d9f88791383a7d099e250ba2a40ed1c3e3c
robo <robolab1902@naver.com>
2022-06-11 23:17:46 +0900

    Unity Game 예제 만들기

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // 플레이어 데이터를 가질 수 있는 변수
+    public PlayerData Pdata;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
