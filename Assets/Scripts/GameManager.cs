2348e39862e6ba1621c2b592d1328400926cdc27
robo <robolab1902@naver.com>
2022-06-13 00:16:32 +0900

    캐릭터 상태창 나오는 것 까지 확인완료

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -35,11 +35,69 @@ public class GameManager : MonoBehaviour
         // 상태창
         Status = GameObject.FindGameObjectsWithTag("Status");
 
+        swordmanText = Status[0].GetComponentsInChildren<Text>();
+        priestText = Status[1].GetComponentsInChildren<Text>();
+        witchText = Status[2].GetComponentsInChildren<Text>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 상태표시창
+        StatusShow();
+    }
+
+    // 상태 표시 함수
+    void StatusShow()
+    {
+        // Dictionary 에서 검사를 찾아야한다.
+        if (D_Player.ContainsKey("검사"))
+        {
+            // 소드맨 오브젝트
+            Player P = D_Player["검사"].GetComponent<Player>();
+            if(P != null)
+            {
+                // 플레이어 잘 가져옴.
+                swordmanText[0].text = P.Pdata.Job;
+                swordmanText[1].text = "레벨            " + P.Pdata.Level;
+                swordmanText[2].text = "경험치          " + P.Pdata.Exp;
+                swordmanText[3].text = "HP              " + P.Pdata.Hp + "/" + P.Pdata.MaxHp;
+                swordmanText[4].text = "MP              " + P.Pdata.Mp + "/" + P.Pdata.MaxMp;
+            }
+        }
+
+        // Dictionary 에서 검사를 찾아야한다.
+        if (D_Player.ContainsKey("신관"))
+        {
+            // 소드맨 오브젝트
+            Player P = D_Player["신관"].GetComponent<Player>();
+            if (P != null)
+            {
+                // 플레이어 잘 가져옴.
+                priestText[0].text = P.Pdata.Job;
+                priestText[1].text = "레벨            " + P.Pdata.Level;
+                priestText[2].text = "경험치          " + P.Pdata.Exp;
+                priestText[3].text = "HP              " + P.Pdata.Hp + "/" + P.Pdata.MaxHp;
+                priestText[4].text = "MP              " + P.Pdata.Mp + "/" + P.Pdata.MaxMp;
+            }
+        }
+
+        // Dictionary 에서 검사를 찾아야한다.
+        if (D_Player.ContainsKey("마법사"))
+        {
+            // 소드맨 오브젝트
+            Player P = D_Player["마법사"].GetComponent<Player>();
+            if (P != null)
+            {
+                // 플레이어 잘 가져옴.
+                witchText[0].text = P.Pdata.Job;
+                witchText[1].text = "레벨            " + P.Pdata.Level;
+                witchText[2].text = "경험치          " + P.Pdata.Exp;
+                witchText[3].text = "HP              " + P.Pdata.Hp + "/" + P.Pdata.MaxHp;
+                witchText[4].text = "MP              " + P.Pdata.Mp + "/" + P.Pdata.MaxMp;
+            }
+        }
     }
 }
