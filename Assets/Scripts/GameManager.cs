6b793cc80e6315286fe611327cf86e5236f56622
robo <robolab1902@naver.com>
2022-06-12 23:48:38 +0900

    GameManager 손보기

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public static GameManager ins;
+    public Dictionary<string, GameObject> D_Player = new Dictionary<string, GameObject>();
 
+    // 플레이어 3개 
+    public GameObject Player1;
+    public GameObject Player2;
+    public GameObject Player3;
+
+    private void Awake()
+    {
+        ins = this;
+    }
 
     // Start is called before the first frame update
     void Start()
